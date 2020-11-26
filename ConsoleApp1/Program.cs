a52947e61390a7c9886e7b627ae7530ad65ffe8d
Hadz_xt61@student.itstep.kz <Hadz_xt61@student.itstep.kz>
2020-11-26 15:46:48 +0600

    Добавьте файлы проекта.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("1,2,3!!!");
+            Console.WriteLine("****++++");
+        }
+    }
+}
