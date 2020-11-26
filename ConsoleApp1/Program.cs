0907daee60419e285ab9ead6f807aa8cecc5130a
Hadz_xt61@student.itstep.kz <Hadz_xt61@student.itstep.kz>
2020-11-26 15:56:32 +0600

    Добавила символы в task2

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -10,5 +10,10 @@ namespace ConsoleApp1
             Console.WriteLine("10,11,12!!!");
             Console.WriteLine("0,1,2,3,4,5,6,7,8,9");
         }
+
+        public static void Text2()
+        {
+            Console.WriteLine("__________");
+        }
     }
 }
